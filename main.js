1acba0cfb4fd927ae407998445451351812e65bc
joaoento <127776557+joaoento@users.noreply.github.com>
2023-09-20 13:16:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function preload(){
+    classifier=ml5.imageClassifier("DoodleNet");
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+background("white");
+canvas.mouseReleased(classifyCanvas);
+}
+function draw (){
+    strokeWeight(13);
+stroke(0);
+if(mouseIsPressed){
+    line(pmouseX,pmouseY,mouseX,mouseY);
+
+}
+}
+function classifyCanvas(){
+    classifier.classify(canvas,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error)
+    }console.log(results);
+    var result=results[0].label;
+    document.getElementById("label").innerHTML="Nome: "+result.replace("_"," ");
+document.getElementById("confidence").innerHTML="Precisão: "+Math.round(results[0].confidence*100)+"%";}
+function clearCanvas(){
+    background("white")
+}
